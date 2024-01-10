f7e73df8d89a107a2f897f78f8a11cd148e28184
Bergson Lima <bergsonsud@gmail.com>
2024-01-10 10:38:37 -0300

    feat(controller): improved actions

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -3,12 +3,32 @@ module Api
     class DnsRecordsController < ApplicationController
       # GET /dns_records
       def index
-        # TODO: Implement this action
+        if index_params[:page].blank?
+          render json: { errors: 'Page param is required' }, status: :unprocessable_entity
+        else
+          render json: DnsRecordsService.new(index_params.slice(:page, :included, :excluded)).call
+        end
       end
 
       # POST /dns_records
       def create
-        # TODO: Implement this action
+        @dns_record = DnsRecord.new(dns_record_params)
+
+        if @dns_record.save
+          render json: { message: 'DNS record created successfully' }, status: :created
+        else
+          render json: { errors: @dns_record.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      protected
+
+      def index_params
+        params.permit(:page, :included, :excluded, dns_record: {})
+      end
+
+      def dns_record_params
+        params.require(:dns_record).permit(:ip, hostnames_attributes: [:hostname])
       end
     end
   end
